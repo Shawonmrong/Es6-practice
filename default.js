3f88cc83d5f87526ddeb5eb8d3d03b13860ad864
Shawon Mrong <shawonmrong391@gmail.com>
2024-02-26 15:52:43 +0600

    first commit

---
+++ default.js
@@ -0,0 +1,24 @@
+
+function add (num1 = 0, num2 = 0) {
+    const result = num1 + num2 ;
+    console.log(num1, num2, result);
+    return result;
+}
+
+const sum = add( 35, 65);
+
+
+function fullName(first , last =''){
+    const full = first + ' ' + last;
+    return full;
+}
+
+
+
+function friends(numbs = []){
+
+}
+
+function person(human = {}){
+    
+}
